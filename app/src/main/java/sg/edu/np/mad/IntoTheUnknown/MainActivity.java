1de175c2a8b424c995c69047661fb536d8af8982
s10208226 <s10208226@connect.np.edu.sg>
2021-07-12 04:01:53 +0800

    Add Timer for Splash Screen

--- app/src/main/java/sg/edu/np/mad/IntoTheUnknown/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/IntoTheUnknown/MainActivity.java
@@ -1,8 +1,12 @@
 package sg.edu.np.mad.IntoTheUnknown;
 
+import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        countDownTimer();
+    }
+
+    private void countDownTimer(){
+        CountDownTimer cdt = new CountDownTimer(2000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(MainActivity.this, searchPage.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        cdt.start();
     }
 }
 //To Do:
