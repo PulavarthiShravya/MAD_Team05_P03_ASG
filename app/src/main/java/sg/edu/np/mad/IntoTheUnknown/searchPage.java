6b9cc6d72a290ca13848ef746421a3d6393a9042
s10205493 <s10205493@connect.np.edu.sg>
2021-07-17 15:22:55 +0800

    added weatherPage class and xml page. Made changes to layout of information page. Need to add constraints in the informationPage layout

--- app/src/main/java/sg/edu/np/mad/IntoTheUnknown/searchPage.java
+++ app/src/main/java/sg/edu/np/mad/IntoTheUnknown/searchPage.java
@@ -2,7 +2,11 @@ package sg.edu.np.mad.IntoTheUnknown;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class searchPage extends AppCompatActivity {
 
@@ -12,9 +16,31 @@ public class searchPage extends AppCompatActivity {
         setContentView(R.layout.activity_search_page);
 
         getSupportActionBar().hide();
+        //variables
+        TextView textView = findViewById(R.id.infoTagName);
+        TextView textView1 = findViewById(R.id.weatherTagName);
+
+        //when user clicks on informationTagName
+        textView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(searchPage.this, informationPage.class);
+                startActivity(intent);
+            }
+        });
+
+        //when user clicks on weatherTagName
+        textView1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(searchPage.this, weatherPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
-//Things to do in this class, user can search available parks using search bar
+//Things to do in this class, user can search available parks using search bar (use API)
 //when user click on any one of the regions to bring user to that particular search region
-// the 3 bottom navigation bar to map to the respective pages
---
+++ app/src/main/java/sg/edu/np/mad/IntoTheUnknown/weatherPage.java
@@ -0,0 +1,18 @@
+package sg.edu.np.mad.IntoTheUnknown;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class weatherPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_page);
+    }
+}
+
+//To-do:
+//weather API, display them as needed
+//intent to search and information pages
