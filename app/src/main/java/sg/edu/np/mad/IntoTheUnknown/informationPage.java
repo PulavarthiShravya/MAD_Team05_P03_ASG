ecd95d526bba1f3ae584fd303cada98414506f0a
s10205493 <s10205493@connect.np.edu.sg>
2021-07-19 12:53:47 +0800

    improved layout for information pages. created rectangle to be used for future (if needed)

--- app/src/main/java/sg/edu/np/mad/IntoTheUnknown/informationPage.java
+++ app/src/main/java/sg/edu/np/mad/IntoTheUnknown/informationPage.java
@@ -2,7 +2,10 @@ package sg.edu.np.mad.IntoTheUnknown;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class informationPage extends AppCompatActivity {
 
@@ -12,10 +15,20 @@ public class informationPage extends AppCompatActivity {
         setContentView(R.layout.activity_information_page);
 
         getSupportActionBar().hide();
+
+        //variables
+        Button button = findViewById(R.id.button);
+        //creating on click action to redirect user to next page
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //creating intent
+                Intent intent = new Intent(informationPage.this, informationPage2.class);
+                //start activity
+                startActivity(intent);
+            }
+        });
     }
 }
 //To Do:
-//main page to show about the weather page
 //need to add screenshot of how weather page looks like
-// 3 navigation "bar" mapping too
-// next button will lead user to how to informationPage2
