5765de22361801c71b29a029cc9949898e4de313
s10205493 <s10205493@connect.np.edu.sg>
2021-07-28 22:57:21 +0800

    added parks for every region to be viewed (recycler View) -done by tricia & shravya

---
+++ app/src/main/java/sg/edu/np/mad/IntoTheUnknown/EastParkListActivity.java
@@ -0,0 +1,77 @@
+package sg.edu.np.mad.IntoTheUnknown;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EastParkListActivity extends AppCompatActivity {
+
+    static List<Park> parksList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_south_park_list);
+
+        String url = "https://tih-api.stb.gov.sg/national-park/v1?location=1.38972941806779%2C103.997184718828&radius=7000&apikey=AuCxF2XWFwNxlSdoWjOYHE71fJqRgi0j";
+        RequestQueue requestQueue = Volley.newRequestQueue(EastParkListActivity.this);
+
+        //making JSON object request
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try{
+                    //go to the data jsonObject
+                    JSONArray dataArray = response.getJSONArray("data");
+
+                    //for loop
+                    for (int i = 0; i<dataArray.length(); i++){
+                        //getting data from the dataArray
+                        JSONObject nameObject = dataArray.getJSONObject(i);
+                        //extracting variables
+                        String nameOfPark = nameObject.getString("name");
+                        parksList.add(new Park(i, nameOfPark));
+                    }
+
+
+
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(EastParkListActivity.this, "No Parks Available", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        ParkAdapter adapter = new ParkAdapter(parksList);
+        LinearLayoutManager mLayoutManager =  new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+    }
+}
--- app/src/main/java/sg/edu/np/mad/IntoTheUnknown/searchPage.java
+++ app/src/main/java/sg/edu/np/mad/IntoTheUnknown/searchPage.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -37,6 +38,51 @@ public class searchPage extends AppCompatActivity {
         TextView textView1 = findViewById(R.id.weatherTagName);
         ImageView searchButton = findViewById(R.id.searchPark);
         TextView searchName = findViewById(R.id.searchParkName);
+        Button north = findViewById(R.id.northButton);
+        Button west = findViewById(R.id.westButton);
+        Button south = findViewById(R.id.southButton);
+        Button east = findViewById(R.id.eastButton);
+
+        //when user clicks on North
+        north.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(searchPage.this, NorthParkListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //when user clicks on West
+        west.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(searchPage.this, WestParkListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //when user clicks on South
+        south.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(searchPage.this, SouthParkListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //when user clicks on East
+        east.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(searchPage.this, EastParkListActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
         //when user clicks on informationTagName
