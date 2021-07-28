9ce09b4e372d51f439f38f9a68bc4d746ed99966
s10205493 <s10205493@connect.np.edu.sg>
2021-07-28 13:10:01 +0800

    changed recycler view layout, added in api to view all parks!! added intent at search image.

--- app/src/main/java/sg/edu/np/mad/IntoTheUnknown/searchPage.java
+++ app/src/main/java/sg/edu/np/mad/IntoTheUnknown/searchPage.java
@@ -6,21 +6,48 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 
 public class searchPage extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_page);
 
+
         getSupportActionBar().hide();
         //variables
         TextView textView = findViewById(R.id.infoTagName);
         TextView textView1 = findViewById(R.id.weatherTagName);
+        ImageView searchButton = findViewById(R.id.searchPark);
+        TextView searchName = findViewById(R.id.searchParkName);
+
+
+        //when user clicks on informationTagName
+        searchButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(searchPage.this, ParkListActivity.class);
+                startActivity(intent);
+            }
+        });
 
         //when user clicks on informationTagName
         textView.setOnClickListener(new View.OnClickListener(){
@@ -42,7 +69,5 @@ public class searchPage extends AppCompatActivity {
             }
         });
     }
-}
 
-//Things to do in this class, user can search available parks using search bar (use API)
-//when user click on any one of the regions to bring user to that particular search region
+}
